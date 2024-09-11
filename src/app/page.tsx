f3869fb12113b9c59ee1ea263a5ab93d1cf72523
Acaj0 <antoniocarlosjr007@hotmail.com>
2024-09-11 17:20:20 -0400

    2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,21 +5,21 @@ import Link from "next/link";
 export default function home() {
   return (
     <div className="flex flex-col min-h-[100dvh]">
-      <header className="px-4 lg:px-6 h-14 flex items-center p-1">
+      <header className="px-4 lg:px-6 h-20 flex items-center p-1">
         <Link
           href="/"
           className="flex items-center justify-center"
           prefetch={false}
         >
           <Image
-          className="mt-10"
-        src={"/4.png"}
-        quality={100}
-        unoptimized={true}
-        height={200}
-        width={200}
-        alt="logo da lumen"
-      />
+            className=""
+            src={"/4.png"}
+            quality={100}
+            unoptimized={true}
+            height={200}
+            width={200}
+            alt="logo da lumen"
+          />
           <span className="sr-only">Home</span>
         </Link>
         <nav className="ml-auto flex gap-4 sm:gap-6">
@@ -40,62 +40,49 @@ export default function home() {
         </nav>
       </header>
       <main className="flex-1">
-        <section className="w-full mt-10 py-12 md:py-24 lg:py-32 bg-black ">
-          <div className="container px-4 md:px-6 bg">
-            <div className="grid gap-6 lg:grid-cols-[1fr_400px] lg:gap-12 xl:grid-cols-[1fr_600px] ">
+        <section className="w-full mt-10 py-12 md:py-24 lg:py-32">
+          <div className="container px-4 md:px-6 bg backdrop-blur-md">
+            <div className="flex gap-6 lg:grid-cols-[1fr_400px] lg:gap-12 xl:grid-cols-[1fr_600px] items-center justify-center">
               <div className="flex flex-col justify-center space-y-4">
-                <div className="space-y-2">
+                <div className="space-y-2 ">
                   <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none">
-                    The complete platform for building the Web
+                    Criação de Sites
                   </h1>
                   <p className="max-w-[600px] text-muted-foreground md:text-xl">
-                    Give your team the toolkit to stop configuring and start
-                    innovating. Securely build, deploy, and scale the best web
-                    experiences.
+                    Construímos soluções inovadoras que impulsionam seu negócio.
+                    Do conceito à escala, tudo com eficiência e segurança,
+                    oferecendo a flexibilidade que você precisa para se destacar
+                    no mercado digital.
                   </p>
                 </div>
                 <div className="flex flex-col gap-2 min-[400px]:flex-row">
                   <Link
                     href="#"
-                    className="inline-flex h-10 items-center justify-center rounded-md bg-primary px-8 text-sm font-medium text-primary-foreground shadow transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
+                    className=" drop-shadow-lg inline-flex h-10 items-center justify-center rounded-md bg-sky-700 px-8 text-sm font-medium text-primary-foreground shadow transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
                     prefetch={false}
                   >
-                    Get Started
-                  </Link>
-                  <Link
-                    href="#"
-                    className="inline-flex h-10 items-center justify-center rounded-md border border-input bg-background px-8 text-sm font-medium shadow-sm transition-colors hover:bg-accent hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
-                    prefetch={false}
-                  >
-                    Contact Sales
+                    Fale com um Especialista
                   </Link>
                 </div>
               </div>
-              <img
-                src="/placeholder.svg"
-                width="550"
-                height="550"
-                alt="Hero"
-                className="mx-auto aspect-video overflow-hidden rounded-xl object-bottom sm:w-full lg:order-last lg:aspect-square"
-              />
+              <img src="/pcs.png" width="300" height="300" alt="Hero" />
             </div>
           </div>
         </section>
-        <section className="w-full py-12 md:py-24 lg:py-32 bg-muted">
+        <section className="w-full py-10 md:py-10 lg:py-10 bg-muted">
           <div className="container px-4 md:px-6">
             <div className="flex flex-col items-center justify-center space-y-4 text-center">
               <div className="space-y-2">
-                <div className="inline-block rounded-lg bg-muted px-3 py-1 text-sm">
-                  New Features
-                </div>
                 <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
-                  Faster iteration. More innovation.
+                  O que nós podemos fazer por você
                 </h2>
                 <p className="max-w-[900px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-                  The platform for rapid progress. Let your team focus on
-                  shipping features instead of managing infrastructure with
-                  automated CI/CD, built-in testing, and integrated
-                  collaboration.
+                  Na Lumen Desenvolvimento Web, ajudamos empresas a se
+                  destacarem no ambiente digital por meio de estratégias
+                  eficazes de SEO (Search Engine Optimization). Com a otimização
+                  adequada de sites e conteúdo, oferecemos às empresas a
+                  oportunidade de melhorar sua visibilidade online e alcançar
+                  mais clientes.
                 </p>
               </div>
             </div>
@@ -111,31 +98,46 @@ export default function home() {
                 <ul className="grid gap-6">
                   <li>
                     <div className="grid gap-1">
-                      <h3 className="text-xl font-bold">Collaboration</h3>
+                      <h3 className="text-xl font-bold">
+                        Sites institucionais
+                      </h3>
                       <p className="text-muted-foreground">
-                        Make collaboration seamless with built-in code review
-                        tools.
+                        Digitalize sua empresa com um site e garanta a sua
+                        presença digital. Não ter um site no mercado atual deixa
+                        sua empresa invisível!
                       </p>
                     </div>
                   </li>
                   <li>
                     <div className="grid gap-1">
-                      <h3 className="text-xl font-bold">Automation</h3>
+                      <h3 className="text-xl font-bold">Loja Virtual</h3>
                       <p className="text-muted-foreground">
-                        Automate your workflow with continuous integration.
+                        Tenha uma plataforma de vendas 24h, de alto custo
+                        benefício para sua empresa, com total suporte durante 1
+                        ano
                       </p>
                     </div>
                   </li>
                   <li>
                     <div className="grid gap-1">
-                      <h3 className="text-xl font-bold">Scale</h3>
+                      <h3 className="text-xl font-bold">Landing Pages</h3>
                       <p className="text-muted-foreground">
-                        Deploy to the cloud with a single click and scale with
-                        ease.
+                        Potencialize suas vendas e transmita autoridade no
+                        mercado com páginas de captura otimizadas para vendas ou
+                        captura de leads.
                       </p>
                     </div>
                   </li>
                 </ul>
+                <div className="flex flex-col gap-2 min-[400px]:flex-row">
+                  <Link
+                    href="#"
+                    className=" drop-shadow-lg inline-flex h-10 items-center justify-center rounded-md bg-sky-700 px-8 text-sm font-medium text-primary-foreground shadow transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
+                    prefetch={false}
+                  >
+                    Fale com um Especialista
+                  </Link>
+                </div>
               </div>
             </div>
           </div>
