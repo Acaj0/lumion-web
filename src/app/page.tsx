9c71fe8ae0cfdabaebdedf3b441b23fb56f00fb2
Acaj0 <antoniocarlosjr007@hotmail.com>
2024-09-13 17:01:16 -0400

    7

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -116,7 +116,9 @@ export default function home() {
                       <path d="M352 256c0 22.2-1.2 43.6-3.3 64l-185.3 0c-2.2-20.4-3.3-41.8-3.3-64s1.2-43.6 3.3-64l185.3 0c2.2 20.4 3.3 41.8 3.3 64zm28.8-64l123.1 0c5.3 20.5 8.1 41.9 8.1 64s-2.8 43.5-8.1 64l-123.1 0c2.1-20.6 3.2-42 3.2-64s-1.1-43.4-3.2-64zm112.6-32l-116.7 0c-10-63.9-29.8-117.4-55.3-151.6c78.3 20.7 142 77.5 171.9 151.6zm-149.1 0l-176.6 0c6.1-36.4 15.5-68.6 27-94.7c10.5-23.6 22.2-40.7 33.5-51.5C239.4 3.2 248.7 0 256 0s16.6 3.2 27.8 13.8c11.3 10.8 23 27.9 33.5 51.5c11.6 26 20.9 58.2 27 94.7zm-209 0L18.6 160C48.6 85.9 112.2 29.1 190.6 8.4C165.1 42.6 145.3 96.1 135.3 160zM8.1 192l123.1 0c-2.1 20.6-3.2 42-3.2 64s1.1 43.4 3.2 64L8.1 320C2.8 299.5 0 278.1 0 256s2.8-43.5 8.1-64zM194.7 446.6c-11.6-26-20.9-58.2-27-94.6l176.6 0c-6.1 36.4-15.5 68.6-27 94.6c-10.5 23.6-22.2 40.7-33.5 51.5C272.6 508.8 263.3 512 256 512s-16.6-3.2-27.8-13.8c-11.3-10.8-23-27.9-33.5-51.5zM135.3 352c10 63.9 29.8 117.4 55.3 151.6C112.2 482.9 48.6 426.1 18.6 352l116.7 0zm358.1 0c-30 74.1-93.6 130.9-171.9 151.6c25.5-34.2 45.2-87.7 55.3-151.6l116.7 0z" />
                     </svg>
                   </div>
-                  <h3 className="mt-2 text-xl font-bold">Performance</h3>
+                  <h3 className="mt-2 text-xl font-bold text-center">
+                    Performance
+                  </h3>
                   <p className="text-muted-foreground">
                     Nossa plataforma é otimizada para desempenho ultrarrápido em
                     qualquer lugar do mundo, garantindo uma experiência fluida
@@ -136,7 +138,7 @@ export default function home() {
                       />
                     </svg>
                   </div>
-                  <h3 className="mt-2 text-xl font-bold">SEO</h3>
+                  <h3 className="mt-2 text-xl font-bold text-center">SEO</h3>
                   <p className="text-muted-foreground">
                     SEO (Search Engine Optimization) é a otimização de sites
                     para melhorar seu posicionamento nos resultados de busca
@@ -155,7 +157,9 @@ export default function home() {
                       <path d="M16 64C16 28.7 44.7 0 80 0L304 0c35.3 0 64 28.7 64 64l0 384c0 35.3-28.7 64-64 64L80 512c-35.3 0-64-28.7-64-64L16 64zM224 448a32 32 0 1 0 -64 0 32 32 0 1 0 64 0zM304 64L80 64l0 320 224 0 0-320z" />
                     </svg>
                   </div>
-                  <h3 className="mt-2 text-xl font-bold">Responsividade</h3>
+                  <h3 className="mt-2 text-xl font-bold text-center">
+                    Responsividade
+                  </h3>
                   <p className="text-muted-foreground">
                     Design responsivo adapta o layout de um site a diferentes
                     tamanhos de tela, garantindo uma exibição otimizada em
@@ -221,11 +225,10 @@ export default function home() {
                   </li>
                   <li>
                     <div className="grid gap-1">
-                      <h3 className="text-xl font-bold">Loja Virtual</h3>
+                      <h3 className="text-xl font-bold">Portfólios online</h3>
                       <p className="text-muted-foreground">
-                        Tenha uma plataforma de vendas 24h, de alto custo
-                        benefício para sua empresa, com total suporte durante 1
-                        ano
+                        Para criativos e profissionais que desejam mostrar seu
+                        trabalho de forma visual e interativa.
                       </p>
                     </div>
                   </li>
@@ -236,6 +239,15 @@ export default function home() {
                         Potencialize suas vendas e transmita autoridade no
                         mercado com páginas de captura otimizadas para vendas ou
                         captura de leads.
+                      </p>
+                    </div>
+                  </li>
+                  <li>
+                    <div className="grid gap-1">
+                      <h3 className="text-xl font-bold">Blogs</h3>
+                      <p className="text-muted-foreground">
+                        Para compartilhar conteúdos, atrair tráfego orgânico e
+                        fortalecer sua presença online.
                       </p>
                     </div>
                   </li>
