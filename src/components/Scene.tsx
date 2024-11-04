539114664f738656fe28f8fb0c7c17d023ed0b43
Acaj0 <antoniocarlosjr007@hotmail.com>
2024-11-04 13:27:25 -0400

    Loading wheel

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { Suspense } from "react"
+import React, { Suspense, useState } from "react"
 import { Canvas } from "@react-three/fiber"
 import { useGLTF, OrbitControls, Environment, PerspectiveCamera } from "@react-three/drei"
 
@@ -9,16 +9,27 @@ function NotebookModel() {
   return <primitive object={scene} />
 }
 
-export default function Scene() {
+function Loader() {
   return (
-    <div className="md:w-[600px] md:h-[600px] w-screen h-[400px] mx-auto">
-      <Canvas>
+    <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+      <div className="w-12 h-12 border-4 border-t-4 border-white rounded-full animate-spin"></div>
+    </div>
+  )
+}
+
+export default function Scene() {
+  const [isLoading, setIsLoading] = useState(true)
+
+  return (
+    <div className="md:w-[600px] md:h-[600px] w-screen h-[400px] mx-auto relative">
+      {isLoading && <Loader />}
+      <Canvas onCreated={() => setIsLoading(false)}>
         <Suspense fallback={null}>
           <PerspectiveCamera makeDefault position={[-5, 2, 3]} />
           <OrbitControls enablePan={false} enableZoom={false} enableRotate={true} />
-          <ambientLight intensity={0.5} />
-          <spotLight position={[10, 10, 10]} angle={1.15} penumbra={1} />
-          <pointLight position={[-10, -10, -10]} />
+          <ambientLight intensity={0.3} />
+          <spotLight position={[10, 10, 10]} angle={0.5} penumbra={1} intensity={0.8} />
+          <pointLight position={[-10, -10, -10]} intensity={0.5} />
           <NotebookModel />
           <Environment preset="studio" />
         </Suspense>
@@ -26,3 +37,5 @@ export default function Scene() {
     </div>
   )
 }
+
+useGLTF.preload("/Lowpoly_Notebook_3.glb")
