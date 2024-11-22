5ba3c9080f20a11ef467c567b05b6a8882b9ffa8
Acaj0 <antoniocarlosjr007@hotmail.com>
2024-11-21 22:37:49 -0400

    1

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -16,7 +16,7 @@ export default function NotFound() {
           <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zm0-384c13.3 0 24 10.7 24 24l0 112c0 13.3-10.7 24-24 24s-24-10.7-24-24l0-112c0-13.3 10.7-24 24-24zM224 352a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z" />
         </svg>
         <h1 className="text-4xl font-bold text-black mb-4">
-          Oops! Página não encontrada
+          Ops! Página não encontrada
         </h1>
         <p className="text-xl text-gray-800 mb-6">
           Parece que nosso código deu um 404 flip!
