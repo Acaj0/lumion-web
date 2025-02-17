5a67e49583d2d8508f30f2c80486e43261f2fefa
antonio.abreu <antonio.abreu@redeflex.com.br>
2025-02-17 15:55:36 -0400

    Alteracao sessao de servicos e atualizacao de numero de telefone

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import NavBar from "@/components/NavBar";
 import Scene from "@/components/Scene";
+import Servicos from "@/components/Servicos";
 import FloatingContactButtons from "@/components/ui/FloatingContactButtons";
 import Image from "next/image";
 import Link from "next/link";
@@ -32,7 +33,7 @@ export default function home() {
                   <div className="flex flex-col gap-2 md:ml-12 min-[400px]:flex-row">
                     <Link
                       target="_blank"
-                      href="https://api.whatsapp.com/send?phone=5565981227631&text=Ol%C3%A1,%20Gostaria%20de%20Fazer%20um%20or%C3%A7amento%20para%20meu%20site!"
+                      href="https://api.whatsapp.com/send?phone=5571982471477&text=Ol%C3%A1,%20Gostaria%20de%20Fazer%20um%20or%C3%A7amento%20para%20meu%20site!"
                       className=" ease-in-out delay-50 hover:-translate-y-1 hover:scale-110 duration-100 gap-2 drop-shadow-lg inline-flex h-10 items-center justify-center rounded-md bg-black/90 px-8 text-sm font-medium text-primary-foreground shadow-md transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
                       prefetch={false}
                     >
@@ -195,121 +196,37 @@ export default function home() {
                 </div>
               </div>
               <Link
+                href="https://api.whatsapp.com/send?phone=5571982471477&text=Ol%C3%A1,%20Gostaria%20de%20Fazer%20um%20or%C3%A7amento%20para%20meu%20site!"
                 target="_blank"
-                href="https://api.whatsapp.com/send?phone=5565981227631&text=Ol%C3%A1,%20Gostaria%20de%20Fazer%20um%20or%C3%A7amento%20para%20meu%20site!"
-                className="shadow-sky-200 gap-2 drop-shadow-lg inline-flex h-10 items-center justify-center rounded-md bg-sky-700 px-8 text-sm font-medium text-primary-foreground shadow-md transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
-                prefetch={false}
+                className="inline-flex items-center justify-center rounded-md bg-sky-700 px-8 py-3 text-sm font-medium text-white shadow transition-colors hover:bg-sky-600 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-sky-700"
               >
                 <svg
                   width={20}
-                  fill="white"
+                  fill="currentColor"
+                  className="mr-2"
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 448 512"
                 >
                   <path d="M380.9 97.1C339 55.1 283.2 32 223.9 32c-122.4 0-222 99.6-222 222 0 39.1 10.2 77.3 29.6 111L0 480l117.7-30.9c32.4 17.7 68.9 27 106.1 27h.1c122.3 0 224.1-99.6 224.1-222 0-59.3-25.2-115-67.1-157zm-157 341.6c-33.2 0-65.7-8.9-94-25.7l-6.7-4-69.8 18.3L72 359.2l-4.4-7c-18.5-29.4-28.2-63.3-28.2-98.2 0-101.7 82.8-184.5 184.6-184.5 49.3 0 95.6 19.2 130.4 54.1 34.8 34.9 56.2 81.2 56.1 130.5 0 101.8-84.9 184.6-186.6 184.6zm101.2-138.2c-5.5-2.8-32.8-16.2-37.9-18-5.1-1.9-8.8-2.8-12.5 2.8-3.7 5.6-14.3 18-17.6 21.8-3.2 3.7-6.5 4.2-12 1.4-32.6-16.3-54-29.1-75.5-66-5.7-9.8 5.7-9.1 16.3-30.3 1.8-3.7 .9-6.9-.5-9.7-1.4-2.8-12.5-30.1-17.1-41.2-4.5-10.8-9.1-9.3-12.5-9.5-3.2-.2-6.9-.2-10.6-.2-3.7 0-9.7 1.4-14.8 6.9-5.1 5.6-19.4 19-19.4 46.3 0 27.3 19.9 53.7 22.6 57.4 2.8 3.7 39.1 59.7 94.8 83.8 35.2 15.2 49 16.5 66.6 13.9 10.7-1.6 32.8-13.4 37.4-26.4 4.6-13 4.6-24.1 3.2-26.4-1.3-2.5-5-3.9-10.5-6.6z" />
                 </svg>
-                Faça um orçamento
+                Fale com um Especialista
               </Link>
             </div>
           </div>
         </section>
-        <section id="3" className="w-full py-10 md:py-10 lg:py-10 bg-muted">
-          <div className="container px-4 md:px-6">
-            <div className="flex flex-col items-center justify-center space-y-4 text-center">
-              <div className="space-y-5">
-                <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
-                  O que nós podemos fazer por você
-                </h2>
-                <p className="max-w-[900px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed text-justify	">
-                  Na Lumen Desenvolvimento Web, ajudamos você a se destacarem no
-                  ambiente digital por meio de estratégias eficazes de SEO
-                  (Search Engine Optimization). Com a otimização adequada de
-                  sites e conteúdo, oferecemos às empresas a oportunidade de
-                  melhorar sua visibilidade online e alcançar mais clientes.
-                </p>
-              </div>
-            </div>
-            <div className="mx-auto grid max-w-5xl items-center gap-6 py-12 lg:grid-cols-2 lg:gap-12">
-              <Image
-                src="/sitesparado.png"
-                width="550"
-                height="310"
-                alt="Image"
-                className="mx-auto overflow-hidden rounded-xl object-cover object-center sm:w-full lg:order-last"
-              />
-              <div className="flex flex-col justify-center space-y-4">
-                <ul className="grid gap-6">
-                  <li>
-                    <div className="grid gap-1">
-                      <h3 className="text-xl font-bold">
-                        Sites institucionais
-                      </h3>
-                      <p className="text-muted-foreground">
-                        Digitalize sua empresa com um site e garanta a sua
-                        presença digital. Não ter um site no mercado atual deixa
-                        sua empresa invisível!
-                      </p>
-                    </div>
-                  </li>
-                  <li>
-                    <div className="grid gap-1">
-                      <h3 className="text-xl font-bold">Portfólios online</h3>
-                      <p className="text-muted-foreground">
-                        Para criativos e profissionais que desejam mostrar seu
-                        trabalho de forma visual e interativa.
-                      </p>
-                    </div>
-                  </li>
-                  <li>
-                    <div className="grid gap-1">
-                      <h3 className="text-xl font-bold">Landing Pages</h3>
-                      <p className="text-muted-foreground">
-                        Potencialize suas vendas e transmita autoridade no
-                        mercado com páginas de captura otimizadas para vendas ou
-                        captura de leads.
-                      </p>
-                    </div>
-                  </li>
-                  <li>
-                    <div className="grid gap-1">
-                      <h3 className="text-xl font-bold">Blogs</h3>
-                      <p className="text-muted-foreground">
-                        Para compartilhar conteúdos, atrair tráfego orgânico e
-                        fortalecer sua presença online.
-                      </p>
-                    </div>
-                  </li>
-                </ul>
-                <div className="flex flex-col gap-2 min-[400px]:flex-row">
-                  <Link
-                    href="https://api.whatsapp.com/send?phone=5565981227631&text=Ol%C3%A1,%20Gostaria%20de%20Fazer%20um%20or%C3%A7amento%20para%20meu%20site!"
-                    target="_blank"
-                    className="gap-2 drop-shadow-lg inline-flex h-10 items-center justify-center rounded-md bg-sky-700 px-8 text-sm font-medium text-primary-foreground shadow transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
-                    prefetch={false}
-                  >
-                    <svg
-                      width={20}
-                      fill="white"
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 448 512"
-                    >
-                      <path d="M380.9 97.1C339 55.1 283.2 32 223.9 32c-122.4 0-222 99.6-222 222 0 39.1 10.2 77.3 29.6 111L0 480l117.7-30.9c32.4 17.7 68.9 27 106.1 27h.1c122.3 0 224.1-99.6 224.1-222 0-59.3-25.2-115-67.1-157zm-157 341.6c-33.2 0-65.7-8.9-94-25.7l-6.7-4-69.8 18.3L72 359.2l-4.4-7c-18.5-29.4-28.2-63.3-28.2-98.2 0-101.7 82.8-184.5 184.6-184.5 49.3 0 95.6 19.2 130.4 54.1 34.8 34.9 56.2 81.2 56.1 130.5 0 101.8-84.9 184.6-186.6 184.6zm101.2-138.2c-5.5-2.8-32.8-16.2-37.9-18-5.1-1.9-8.8-2.8-12.5 2.8-3.7 5.6-14.3 18-17.6 21.8-3.2 3.7-6.5 4.2-12 1.4-32.6-16.3-54-29.1-75.5-66-5.7-9.8 5.7-9.1 16.3-30.3 1.8-3.7 .9-6.9-.5-9.7-1.4-2.8-12.5-30.1-17.1-41.2-4.5-10.8-9.1-9.3-12.5-9.5-3.2-.2-6.9-.2-10.6-.2-3.7 0-9.7 1.4-14.8 6.9-5.1 5.6-19.4 19-19.4 46.3 0 27.3 19.9 53.7 22.6 57.4 2.8 3.7 39.1 59.7 94.8 83.8 35.2 15.2 49 16.5 66.6 13.9 10.7-1.6 32.8-13.4 37.4-26.4 4.6-13 4.6-24.1 3.2-26.4-1.3-2.5-5-3.9-10.5-6.6z" />
-                    </svg>
-                    Fale com um Especialista
-                  </Link>
-                </div>
-              </div>
-            </div>
-          </div>
+        <section id="3">
+          <Servicos></Servicos>
         </section>
-
         <section id="team" className="w-full pt-12 md:pt-24 lg:pt-32 bg-white">
           <div className="container px-4 md:px-6">
             <div className="flex flex-col items-center justify-center space-y-4 text-center">
               <div className="space-y-2">
-                <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">Nossa Equipe</h2>
+                <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
+                  Nossa Equipe
+                </h2>
                 <p className="max-w-[900px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-                  Conheça os profissionais dedicados por trás da Lumen Desenvolvimento Web.
+                  Conheça os profissionais dedicados por trás da Lumen
+                  Desenvolvimento Web.
                 </p>
               </div>
             </div>
@@ -353,9 +270,9 @@ export default function home() {
                 </p>
               </div>
             </div>
-            <div className="mx-auto grid max-w-5xl items-start gap-6 py-12 lg:grid-cols-2 lg:gap-12">
+            <div className="mx-auto grid items-start gap-6 py-12 lg:grid-cols-3 lg:gap-12">
               <div className="flex flex-col justify-center space-y-4">
-                <div className="bg-white rounded-lg shadow-md min-h-80 p-6">
+                <div className="bg-white rounded-lg shadow-md min-h-[380px] p-6">
                   <a
                     target="_blank"
                     href="https://www.google.com/search?client=opera-gx&q=Lumen+Desenvolvimento+Web&sourceid=opera&ie=UTF-8&oe=UTF-8#lrd=0xdadd90ab3e655c3:0x8924dfa94f53033c,1,,,,"
@@ -400,7 +317,7 @@ export default function home() {
                 </div>
               </div>
               <div className="flex flex-col justify-center space-y-4">
-                <div className="bg-white rounded-lg min-h-80 shadow-md p-6">
+                <div className="bg-white rounded-lg min-h-[380px] shadow-md p-6">
                   <a
                     target="_blank"
                     href="https://www.google.com/search?client=opera-gx&q=Lumen+Desenvolvimento+Web&sourceid=opera&ie=UTF-8&oe=UTF-8#lrd=0xdadd90ab3e655c3:0x8924dfa94f53033c,1,,,,"
@@ -495,7 +412,7 @@ export default function home() {
                 </div>
               </div>
               <div className="flex flex-col justify-center space-y-4">
-                <div className="bg-white rounded-lg min-h-[355.84px] shadow-md p-6">
+                <div className="bg-white rounded-lg min-h-80 shadow-md p-6">
                   <a
                     target="_blank"
                     href="https://www.google.com/search?client=opera-gx&q=Lumen+Desenvolvimento+Web&sourceid=opera&ie=UTF-8&oe=UTF-8#lrd=0xdadd90ab3e655c3:0x8924dfa94f53033c,1,,,,"
